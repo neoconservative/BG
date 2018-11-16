4ea36ae2697ced596a0f69ca182bae95d8606ff1
Sergey <goblinnefs@yandex.ru>
2018-11-16 17:53:25 +0600

    v0.2

--- js/buttonup.js
+++ js/buttonup.js
@@ -7,7 +7,22 @@ $(document).ready(function($) {
         $(this).addClass('active');
         $('.technologiestab_content').hide();
 
-        var selectTab = $(this).find('a').attr("href");
+        var selectTab = $(this).find('a').attr("data-target");
+
+        $(selectTab).fadeIn();
+    });
+});
+
+$(document).ready(function($) {
+    $('.value_content').hide();
+    $('.value_content:first').show();
+    $('.valuetabs li:first').addClass('active');
+    $('.valuetabs li').click(function(event) {
+        $('.valuetabs li').removeClass('active');
+        $(this).addClass('active');
+        $('.value_content').hide();
+
+        var selectTab = $(this).find('a').attr("data-target");
 
         $(selectTab).fadeIn();
     });
